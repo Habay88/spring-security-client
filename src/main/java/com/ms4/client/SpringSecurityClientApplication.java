64b7e502e8c5576a05177955fb40960bbf0ce7ee
Habay <biodun.banjoko@yahoo.com>
2023-04-10 17:57:09 +0100

    first commit

---
+++ src/main/java/com/ms4/client/SpringSecurityClientApplication.java
@@ -0,0 +1,13 @@
+package com.ms4.client;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringSecurityClientApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringSecurityClientApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/ms4/client/repository/PasswordResetTokenRepository.java
@@ -0,0 +1,12 @@
+package com.ms4.client.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.ms4.client.entity.PasswordResetToken;
+
+@Repository
+public interface PasswordResetTokenRepository extends
+        JpaRepository<PasswordResetToken,Long> {
+    PasswordResetToken findByToken(String token);
+}
